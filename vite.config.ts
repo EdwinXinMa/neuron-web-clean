43bf1e5d96d3146878cf1d2b089711cafd696e08
阿威 <deploy@n3lite.com>
2026-03-22 15:46:57 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://139.199.71.144:8080',
         changeOrigin: true,
       },
     },
